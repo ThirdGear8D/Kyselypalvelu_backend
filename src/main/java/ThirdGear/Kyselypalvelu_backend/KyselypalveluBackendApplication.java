d1bc65c454a3a37a5dadcb42eedfd009de0c73e1
Maarit Laakso <maarit.laakso2@myy.haaga-helia.fi>
2022-03-31 13:31:14 +0300

    loput

---
+++ src/main/java/ThirdGear/Kyselypalvelu_backend/KyselypalveluBackendApplication.java
@@ -0,0 +1,13 @@
+package ThirdGear.Kyselypalvelu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselypalveluBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselypalveluBackendApplication.class, args);
+	}
+
+}
